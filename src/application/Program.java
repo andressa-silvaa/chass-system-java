79c06b7de403284c0f9f9bf3d6824e7697b5c068
Andressa Silva <silva.13579.2468@gmail.com>
2023-06-26 18:41:20 -0300

    Chess layer and printing the board 2

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,7 +6,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		 ChessMatch chessMatch = new ChessMatch();
-		 UI.printBoard(chessMatch.getpieces());
+		 UI.printBoard(chessMatch.getPieces());
 
 	}
 
