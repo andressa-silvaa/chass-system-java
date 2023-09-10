a37476a37bb1428d8a9480235466a09ebd17b092
Andressa Silva <silva.13579.2468@gmail.com>
2023-09-10 09:19:54 -0300

    Implementing possible moves of Rook

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,17 +1,16 @@
 package boardgame;
-
 public abstract class Piece {
+
 	protected Position position;
 	private Board board;
 	
 	public Piece(Board board) {
 		this.board = board;
-		
+		position = null;
 	}
 
 	protected Board getBoard() {
 		return board;
-		
 	}
 	
 	public abstract boolean[][] possibleMoves();
@@ -19,16 +18,16 @@ public abstract class Piece {
 	public boolean possibleMove(Position position) {
 		return possibleMoves()[position.getRow()][position.getColumn()];
 	}
+	
 	public boolean isThereAnyPossibleMove() {
-		boolean  [][] mat = possibleMoves();
-		for(int i = 0; i < mat.length;i++) {
-			for(int j = 0; j < mat.length;j++) {
-				if(mat[i][j]) {
+		boolean[][] mat = possibleMoves();
+		for (int i=0; i<mat.length; i++) {
+			for (int j=0; j<mat.length; j++) {
+				if (mat[i][j]) {
 					return true;
 				}
 			}
 		}
 		return false;
 	}
-	
 }
